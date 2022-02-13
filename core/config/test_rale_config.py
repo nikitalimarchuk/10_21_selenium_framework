97facbe18be5ffbcb36b11a6578b877f1f1a5885
Nikita Limarchuk <nikita.limarchuk@gmail.com>
2022-02-14 00:34:45 +0200

    Add diagram into the project

--- core/config/test_rale_config.py
+++ core/config/test_rale_config.py
@@ -3,7 +3,5 @@ from ..singleton import Singleton
 
 class TestRailConfig(Singleton):
     def __init__(self):
-        super().__init__()
-
         self.host = "https://testrail.com"
         self.access_token = "sdklfjsdkfjlskdjflskdjflkj"
